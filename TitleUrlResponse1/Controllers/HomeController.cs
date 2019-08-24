e09cadf9d7d2589b28f9b6e5cfed620648a433e7
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-24 21:48:12 +0300

    Add project files.

---
+++ TitleUrlResponse1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using TitleUrlResponse1.Models;
+
+namespace TitleUrlResponse1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        Info info = new Info();
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public string GetUrl(string text)
+        {
+            // определяем title url-страницы
+            string url = text;
+            string address = new WebClient().DownloadString(url);
+            string title = Regex.Match(address, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
+
+            //проверяем кодировку источника 
+
+            return title;
+            //string[] urls = text.Split(' ', ',', '.', '\t');
+            //return urls;
+        }
+    }
+}
---
+++ TitleUrlResponse1/Models/Info.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace TitleUrlResponse1.Models
+{
+    public class Info
+    {
+        [UrlValidator(ErrorMessage = "please_enter_valid_ftp_url")]
+        public string Address { get; set; }
+
+    }
+
+    public class UrlValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var x = value.ToString();
+                if (Regex.IsMatch(x, @"^http:\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$", RegexOptions.IgnoreCase))
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            else
+            {
+                return new ValidationResult("Please enter some value");
+            }
+        }
+    }
+
+}
