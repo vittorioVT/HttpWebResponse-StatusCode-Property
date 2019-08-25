5e84fabfea60e39f9c00943ad2a4ee88315f3f5c
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-25 14:45:58 +0300

    Created function GetCorrectUrl
    Function to correctly determine the URL entered by  user

--- TitleUrlResponse1/Controllers/HomeController.cs
+++ TitleUrlResponse1/Controllers/HomeController.cs
@@ -15,28 +15,114 @@ namespace TitleUrlResponse1.Controllers
     {
 
         Info info = new Info();
-        
+        public string url;
+        public string address;
+
+
         [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
 
+        public ActionResult About(string text)
+        {
+            if (GetCorrectUrl(text) == null)
+            {
+                ViewBag.MessageAboutUrl = "Пожалуйста, введите корректный адрес";
+            }
+            else
+            {
+                url = GetCorrectUrl(text);
+                address = new WebClient().DownloadString(url);
+                string title = Regex.Match(address, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
+                ViewBag.TitleUrl = title;
+            }
+            return View();
+        }
+
+         // проверяем, корректен ли адрес, который ввел пользователь
+        public string GetCorrectUrl(string text)
+        {
+            if (text != null)
+            {
+                if (Regex.IsMatch(text, @"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$", RegexOptions.IgnoreCase))
+                {
+                    return text;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
+        
 
         [HttpPost]
         public string GetUrl(string text)
         {
             // определяем title url-страницы
-            string url = text;
-            string address = new WebClient().DownloadString(url);
+            if (GetCorrectUrl(text) == null)
+            {
+                ViewBag.MessageAboutUrl = "Пожалуйста, введите корректный адрес";
+            }
+            else
+            {
+                url = GetCorrectUrl(text);
+            }
+             url = GetCorrectUrl(text);
+            address = new WebClient().DownloadString(url);
             string title = Regex.Match(address, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
 
             //проверяем кодировку источника 
 
             return title;
+            
+            
             //string[] urls = text.Split(' ', ',', '.', '\t');
             //return urls;
         }
+
+        //public string[] GetStatusCode(string text)
+        //{
+        //    List<Info> collectionList = new List<Info>()
+        //    {
+        //        new Info{ Address="", Title="", LastRequest=, LastStatus="", RequestCount= },
+        //        new Info{ Address="", Title="", LastRequest=, LastStatus="", RequestCount= },
+        //        new Info{ Address="", Title="", LastRequest=, LastStatus="", RequestCount= },
+        //        new Info{ Address="", Title="", LastRequest=, LastStatus="", RequestCount= }
+        //    };
+
+        //    //return collectionList;
+
+        //}
+
+        public Encoding GetCorrectEncoding()
+        {
+
+            return null;
+        }
+
+
+        
+
+
+
+
+
     }
 }
--- TitleUrlResponse1/Models/Info.cs
+++ TitleUrlResponse1/Models/Info.cs
@@ -9,32 +9,13 @@ namespace TitleUrlResponse1.Models
 {
     public class Info
     {
-        [UrlValidator(ErrorMessage = "please_enter_valid_ftp_url")]
         public string Address { get; set; }
-
+        public string Title { get; set; }
+        public string LastStatus { get; set; }
+        public DateTime LastRequest { get; set; }
+        public int RequestCount { get; set; }
     }
 
-    public class UrlValidator : ValidationAttribute
-    {
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-                var x = value.ToString();
-                if (Regex.IsMatch(x, @"^http:\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$", RegexOptions.IgnoreCase))
-                {
-                    return ValidationResult.Success;
-                }
-                else
-                {
-                    return new ValidationResult(ErrorMessage);
-                }
-            }
-            else
-            {
-                return new ValidationResult("Please enter some value");
-            }
-        }
-    }
+    
 
 }
