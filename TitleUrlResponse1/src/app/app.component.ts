1f524bc5cad0be6160400240a96166a367108483
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-28 21:49:34 +0300

    Added library of Angular Material
    Added library and components for Angular

---
+++ TitleUrlResponse1/src/app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'titleurl',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.compontnet.css']
+})
+export class AppComponent {
+    title: string = "Hello!";
+
+};
